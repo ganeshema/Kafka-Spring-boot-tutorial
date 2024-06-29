f4f68295b4239861129948fdabd7df1fdecd0aee
Ganesh GC <ganeshlema2021@gmail.com>
2024-06-29 17:02:21 -0500

    creation of kafka topic completed

---
+++ src/main/java/com/ganeshgc/apache_kafka_tutorial/ApacheKafkaTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.ganeshgc.apache_kafka_tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApacheKafkaTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApacheKafkaTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ganeshgc/apache_kafka_tutorial/ApacheKafkaTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ganeshgc.apache_kafka_tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApacheKafkaTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
